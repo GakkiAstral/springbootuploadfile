ed51c075c9c8926e31d4de553c44cb5eeeae12e8
tongliang <852481695@qq.com>
2021-06-16 18:42:51 +0800

    Initial commit

---
+++ src/main/java/com/bjsxt/springbootuploadfile/SpringbootuploadfileApplication.java
@@ -0,0 +1,13 @@
+package com.bjsxt.springbootuploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootuploadfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootuploadfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjsxt/springbootuploadfile/SpringbootuploadfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjsxt.springbootuploadfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootuploadfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
